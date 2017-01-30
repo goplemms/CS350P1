203cc75abcdbe24e0235edc98aea6bf65627a375
goplemms <isong2@binghamton.edu>
2017-01-29 21:08:36 -0500

    Obssesive Upload 2.29..9..08

--- sorter.c
+++ sorter.c
@@ -6,9 +6,9 @@
 
 #define TRUE 1
 #define FALSE 0
-#define FREEFILE if(i_flag){free(input_file);}if(o_flag){free(output_file);}if(c_flag){free(count_file);}
+#define FREEFILE if(i_flag)free(input_file);if(o_flag)free(output_file);if(c_flag)free(count_file);
 
-unsigned int* getUsername();
+char* getUsername();
 int compareInt(const void * a, const void * b);
 
 //side note
@@ -33,14 +33,17 @@ int main(int argc, char *argv[], char *envp[]){
   
   //Input-File-Name
   unsigned short int i_flag = FALSE;
+  FILE* input =NULL;
   char* input_file;
   
   //Output-File-Name
   unsigned short int o_flag = FALSE;
+  FILE* output = NULL;
   char* output_file;
 
   //Count-File-Name
   unsigned short int c_flag = FALSE;
+  FILE* count = NULL;
   char* count_file;
   
   
@@ -57,6 +60,10 @@ int main(int argc, char *argv[], char *envp[]){
       }
     case 'n':
       {
+	if(n_flag){
+	  fprintf(stderr, "Duplicate %c option. Ignoring\n", optopt);
+	  break;
+	}
 	n_flag = TRUE;
 	num_int = 0;
 	for(const char *c = optarg; isdigit(*c) != 0; c++){
@@ -65,13 +72,17 @@ int main(int argc, char *argv[], char *envp[]){
 	}
 	if(num_int <= 0 && num_int >= 1000000){
 	  fprintf(stderr, "The -n option should have an integer argument that is greater than 0 and less than 1000000\n");
-	  FREEFILE
-	    exit(1);
+	  FREEFILE;
+	  exit(1);
 	}
 	break;
       }
     case 'm':
       {
+	if(m_flag){
+	  fprintf(stderr, "Duplicate %c option. Ignoring\n", optopt);
+	  break;
+	}
 	m_flag = TRUE;
 	min_int = 0;
 	for(const char *c = optarg; isdigit(*c) != 0; c++){
@@ -80,17 +91,21 @@ int main(int argc, char *argv[], char *envp[]){
 	}
 	if(min_int < 1){
 	  fprintf(stderr, "The -m option should have an integer argument that is greater than 0\n");
-	  FREEFILE
-	    exit(1);
+	  FREEFILE;
+	  exit(1);
 	}else if(M_flag && min_int > max_int){
 	  fprintf(stderr, "The -M argument should have an integer argument that is greater than the -m argument\n");
-	  FREEFILE
-	    exit(1);
+	  FREEFILE;
+	  exit(1);
 	}
 	break;
       }
     case 'M':
       {
+	if(M_flag){
+	  fprintf(stderr, "Duplicate %c option. Ignoring\n", optopt);
+	  break;
+	}
 	M_flag = TRUE;
 	max_int = 0;
 	for(const char *c = optarg; isdigit(*c) != 0; c++){
@@ -99,83 +114,96 @@ int main(int argc, char *argv[], char *envp[]){
 	}
 	if(max_int >= 1000000){
 	  fprintf(stderr, "The -M option should have an integer argument that is greater than 0 and less than 1000000\n");
-	  FREEFILE
-	    exit(1);
+	  FREEFILE;
+	  exit(1);
 	}else if(max_int < min_int){
 	  fprintf(stderr, "The -M argument should have an integer argument that is greater than the -m argument\n");
-	  FREEFILE
-	    exit(1);
+	  FREEFILE;
+	  exit(1);
 	}
 	break;
       }
     case 'i':
       {
-	i_flag = TRUE;
+	if(i_flag){
+	  fprintf(stderr, "Duplicate %c option. Ignoring\n", optopt);
+	  break;
+	}
 	if(optarg == NULL){
 	  fprintf(stderr,"There must be a path for the input file after -i\n");
-	  FREEFILE
-	    exit(1);
+	  FREEFILE;
+	  exit(1);
 	}
 	if(access(optarg, F_OK) == -1){
-	  fprintf(stderr,"The file %s does not exist", optarg);
-	  FREEFILE
-	    exit(1);
+	  fprintf(stderr,"The file %s does not exist\n", optarg);
+	  FREEFILE;
+	  exit(1);
 	}
 	input_file = malloc(sizeof(char) * strlen(optarg));
 	input_file = strdup(optarg);
+	
+	i_flag = TRUE;
 	break;
       }
     case 'o':
       {
-	o_flag = TRUE;
+	if(o_flag){
+	  fprintf(stderr, "Duplicate %c option. Ignoring\n", optopt);
+	  break;
+	}
 	if(optarg == NULL){
 	  fprintf(stderr,"There must be a path for the output file after -o\n");
-	  FREEFILE
-	    exit(1);
+	  FREEFILE;
+	  exit(1);
 	}
 	if(access(optarg, F_OK) == -1){
-	  fprintf(stderr,"The file %s does not exist", optarg);
-	  FREEFILE
-	    exit(1);
+	  fprintf(stderr,"The file %s does not exist\n", optarg);
+	  FREEFILE;
+	  exit(1);
 	}
 	output_file = malloc(sizeof(char) * strlen(optarg));
 	output_file = strdup(optarg);
+	o_flag = TRUE;
 	break;
       }
     case 'c':
       {
-	c_flag = TRUE;
+	if(c_flag){
+	  fprintf(stderr, "Duplicate %c option. Ignoring\n", optopt);
+	  break;
+	}
 	if(optarg == NULL){
 	  fprintf(stderr,"There must be a path for the count file after -c\n");
-	  FREEFILE
-	    exit(1);
+	  FREEFILE;
+	  exit(1);
 	}
 	if(access(optarg, F_OK) == -1){
-	  fprintf(stderr,"The file %s does not exist", optarg);
-	  FREEFILE
-	    exit(1);
+	  fprintf(stderr,"The file %s does not exist\n", optarg);
+	  FREEFILE;
+	  exit(1);
 	}
 	count_file = malloc(sizeof(char) * strlen(optarg));
 	count_file = strdup(optarg);
+	c_flag = TRUE;
 	break;
       }
     case '?':
       //Getopt already returns an informative string
       //fprintf(stderr, "Unrecognized option: -%c\n", optopt);
       fprintf(stderr, "Cannot pass in values through the command line\n");
-      FREEFILE
-	exit(1);
+      FREEFILE;
+      exit(1);
      
     case ':':
       //Getopt already returns an informative string
       //fprintf(stderr, "Option -%c requires an argument\n", optopt);
       fprintf(stderr, "Cannot pass in values through the command line\n");
-      FREEFILE
-	exit(1);
+      FREEFILE;
+      exit(1);
     default:
       {
 	fprintf(stderr, "Cannot pass in values through the command line\n");
-	FREEFILE
+	FREEFILE;
 	exit(1);
 	break;
       }
@@ -183,24 +211,34 @@ int main(int argc, char *argv[], char *envp[]){
     opt = getopt(argc, argv, "un:m:M:i:o:c:");
   }
 
-  
   //Create unsorted matrix
   int* matrix = malloc(sizeof(int) * num_int);
   int saved_count = 0;
-  if(!i_flag){
-    int* eval = malloc(sizeof(int));
-    int check_scan = scanf("%d", eval);
-    for(; saved_count < num_int && check_scan == 1; saved_count++){
-      if(*eval >= min_int && *eval <= max_int){
-	matrix[saved_count] = *eval;
-      }
-      check_scan = scanf("%d", eval);
-    }
-    free(eval);
-  }else{
-    //read from input_file path
+  int* eval = malloc(sizeof(int));
+
+  if(i_flag){
+    input = fopen(input_file, "r");
   }
- 
+  
+  int check_scan = (i_flag)?fscanf(input,"%d", eval):scanf("%d", eval);
+  
+  for(; saved_count < num_int; saved_count++){
+    if(check_scan != 1){
+      fprintf(stderr,"There should be at least %d values being processed\n", num_int);
+      FREEFILE;
+      free(matrix);
+      free(eval);
+      exit(1);
+    }
+    if(*eval >= min_int && *eval <= max_int){
+      matrix[saved_count] = *eval;
+    }
+    check_scan = (i_flag)?fscanf(input,"%d", eval):scanf("%d", eval);
+  }
+
+  free(eval);
+  fclose(input);
+  
   realloc(matrix, sizeof(int) * (saved_count));
 
     
@@ -208,33 +246,72 @@ int main(int argc, char *argv[], char *envp[]){
     
   qsort(matrix,saved_count,sizeof(int), compareInt);
 
+  /*  
+  for(int i = 0; i < saved_count; i++){
+    printf("Sorted Order: %d\n", matrix[i]);
+    sleep(1);
+  }
+  */
+
 
   //Seek values
-  unsigned int *username_array = getUsername();
+  unsigned int username_length = (unsigned int)strlen(getenv("USER"));
+  char* username = getUsername();
+  unsigned int* matches = malloc(sizeof(unsigned int) * (username_length));
+  memset(matches, 0, sizeof(unsigned int) * username_length);
 
+  for(int i = 0; i < saved_count; i++){
+    for(int j = 0; j < username_length; j++){
+      if(matrix[i] == username[j]){
+	matches[j]++;
+      }
+    }
+  }
 
+  /*
+  for(int i = 0; i < username_length; i++){
+    printf("Character %c occurs %u times\n",username[i], matches[i]);
+    sleep(1);
+  }
+  */
 
+  if(c_flag){
+    count = fopen(count_file, "w");
+  }
+  
+  for(int i = 0; i < username_length; i++){
+    if(c_flag){
+      fprintf(count, "%c\t%u\t%u\n", username[i], username[i], matches[i]);
+    }else{
+      printf("%c\t%u\t%u\n", username[i], username[i], matches[i]);
+    }
+  }
+  
   //Return output
   
   
   //Emancipation
-  FREEFILE
-  free(matrix);
-  free(username_array);
-  
+  FREEFILE;
+    
+  if(i_flag){
+    fclose(input);
+  }
+  if(o_flag){
+    //fclose(output);
+  }
+  if(c_flag){
+    //fclose(count);
+  }
+    free(matrix);
+  free(username);
   return 0;
 }
 
-unsigned int* getUsername(){
+char* getUsername(){
   unsigned int name_length = (unsigned int)strlen(getenv("USER"));
-  unsigned int *username_array = malloc(sizeof(unsigned int) * name_length);
   char* username = malloc(sizeof(char) * name_length);
   strcpy(username,getenv("USER"));
-  for(unsigned int i = 0; i < name_length; i++){
-    username_array[i] = (int)username[i];
-  }
-  free(username);
-  return username_array;
+  return username;
 }
 
 int compareInt(const void * a, const void * b){
